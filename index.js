c15e371f0dae3fb2a3c5694e083f84da8ee8a683
Sune Simonsen <sune@we-knowhow.dk>
2017-11-17 13:54:40 +0100

    Simplify the code and make sure multiple versions of appendStyles works together

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var styleElements = {};
-var head;
-
 function appendStyles(options) {
   options = options || {};
 
@@ -13,8 +10,7 @@ function appendStyles(options) {
     throw new Error('You need to specify the id of the style element');
   }
 
-  styleElements[id] = styleElements[id] || createStyleElement(options);
-  var styleElement = styleElements[id];
+  var styleElement = document.getElementById(id) || createStyleElement(options);
 
   // strip potential UTF-8 BOM if css was read from a file
   if (css.charCodeAt(0) === 0xfeff) {
@@ -30,20 +26,16 @@ function appendStyles(options) {
   return styleElement;
 }
 
-function getElementById(id) {
-  return (styleElements[id] = styleElements[id] || document.getElementById(id));
-}
-
 function createStyleElement(options) {
   var styleElement = document.createElement('style');
   styleElement.setAttribute('type', 'text/css');
   styleElement.setAttribute('id', options.id);
 
-  head = head || document.querySelector('head');
+  var head = document.head
 
   var target = head.childNodes[0];
-  var before = options.before && getElementById(options.before);
-  var after = options.after && getElementById(options.after);
+  var before = options.before && document.getElementById(options.before);
+  var after = options.after && document.getElementById(options.after);
 
   target = before || (after && after.nextSibling) || target;
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -11,6 +11,10 @@ var expect = require('unexpected')
 var appendStyles = require('..');
 
 describe('append-styles', function() {
+  beforeEach(() => {
+    document.head.innerHTML = ''
+  })
+
   it('appends styles in the right order', function() {
     appendStyles({
       css: 'body { background:red; }',
@@ -55,4 +59,59 @@ describe('append-styles', function() {
       '<style id="module-b">body { background:red; }h1 { color:black; }</style>'
     ]);
   });
+
+  describe('when the DOM already contains style tags with the same id', () => {
+    beforeEach(() => {
+      document.head.innerHTML = [
+        '<style id="module-a">body { background:yellow; }p { color:gray; }</style>',
+        '<style id="module-b">body { background:green; }p { color:white; }</style>'
+      ].join('')
+    })
+
+    it('appends to the existing style tag', () => {
+      appendStyles({
+        css: 'body { background:red; }',
+        id: 'module-b',
+        after: 'module-a'
+      });
+
+      expect(document, 'to contain styles', [
+        '<style id="module-a">body { background:yellow; }p { color:gray; }</style>',
+        '<style id="module-b">body { background:green; }p { color:white; }body { background:red; }</style>'
+      ]);
+
+      appendStyles({
+        css: 'body { background:blue; }',
+        id: 'module-a',
+        before: 'module-b'
+      });
+
+      expect(document, 'to contain styles', [
+        '<style id="module-a">body { background:yellow; }p { color:gray; }body { background:blue; }</style>',
+        '<style id="module-b">body { background:green; }p { color:white; }body { background:red; }</style>'
+      ]);
+
+      appendStyles({
+        css: 'h1 { color:papayawhip; }',
+        id: 'module-a',
+        before: 'module-b'
+      });
+
+      expect(document, 'to contain styles', [
+        '<style id="module-a">body { background:yellow; }p { color:gray; }body { background:blue; }h1 { color:papayawhip; }</style>',
+        '<style id="module-b">body { background:green; }p { color:white; }body { background:red; }</style>'
+      ]);
+
+      appendStyles({
+        css: 'h1 { color:black; }',
+        id: 'module-b',
+        after: 'module-a'
+      });
+
+      expect(document, 'to contain styles', [
+        '<style id="module-a">body { background:yellow; }p { color:gray; }body { background:blue; }h1 { color:papayawhip; }</style>',
+        '<style id="module-b">body { background:green; }p { color:white; }body { background:red; }h1 { color:black; }</style>'
+      ]);
+    })
+  })
 });
